85d29881fbb3d98ddfbe24a19838fa05297750df
unknown <sudrajatrizal1@gmail.com>
2026-05-11 09:34:53 +0700

    drag and drop on mobile

--- src/components/home/Column.tsx
+++ src/components/home/Column.tsx
@@ -88,7 +88,7 @@ export default function Column({
         <div
           ref={setNodeRef}
           className="flex flex-col gap-4 mt-4 transition-all duration-300"
-          style={{ touchAction: "auto" }}
+          style={{ touchAction: "manipulation" }}
         >
           {tasks.map((task) => (
             <TaskCard
